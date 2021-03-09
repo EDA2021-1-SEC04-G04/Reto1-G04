2087201943daf9362e5b2c98054106f16f424b7b
SergioPardo55 <s.pardog@uniandes.edu.co>
2021-03-09 01:00:49 -0500

    Corrección sort y trending

--- App/view.py
+++ App/view.py
@@ -25,6 +25,7 @@ import sys
 import controller
 import time
 from DISClib.ADT import list as lt
+from DISClib.ADT import queue as qu
 assert cf
 
 default_limit = 1000
@@ -53,16 +54,136 @@ def loadData(catalog,tipo):
     """
     controller.loadData(catalog,tipo)
 
-#def imprimir_primer_elemento(catalog):
-#    elementos_pais = catalog['elements'][0]['videos']['elements'][0]
- #   print(elementos_pais)
+def views_country_category(catalog,country,num_countries,category):
+    """
+    Cumple el requerimiento número 1 del reto buscando los videos 
+    con más views en un país con una categoría dada. Imprime el 
+    número de videos pedido por el usuario.
+    """
+    countries = controller.look_for_country(catalog,country)
+    categories = controller.look_for_category(countries,category)
+    printVideosMostViews(categories,country,num_countries)
+    
+def trending_country(catalog, country):
+    """
+    Cumple el requerimiento número 2 del reto buscando el video
+    con más tiempo trending en un país. Imprime este mismo.
+    """
+    countries = controller.look_for_country(catalog,category)
+    most_trending = controller.look_for_most_trending(countries)
+    print_most_trending_categories(most_trending, category)
 
+def trending_category(catalog, category):
+    """
+    Cumple el requerimiento número 3 del reto buscando el video
+    con más tiempo trending en una categoría. Imprime este mismo.
+    """
+    categories = controller.look_for_category(catalog,category)
+    most_trending = controller.look_for_most_trending(categories)
+    print_most_trending_categories(most_trending, category)
+
+def likes_country_tag(num_videos,country,tag,catalog_videos):
+    """
+    Cumple el requerimiento número 4 del reto buscando los videos 
+    con más likes en un país con un tag específico. Imprime el 
+    número de videos pedido por el usuario.
+    """
+    countries = controller.look_for_country(catalog_videos,country)
+    tags = controller.look_for_tags(countries,tag)
+    tags_by_likes = controller.videos_by_likes(tags)
+    print_video_tags(tags_by_likes,num_videos,country,tag)
+
+def printVideosMostViews(videos,country,num_countries):
+    """
+    Imprime los n videos con más vistas especficando, su trending date,
+    título, título del canal, tiempo de publicación, vistas likes y dislikes.
+    """
+    size = lt.size(videos)
+    if size:
+        print(' Estos son los videos con más views en ' + country.title() + ":")
+        primera_linea = "{0:^13}{1:^100}{2:^25}{3:^25}{4:^15}{5:^10}{6:^10}".format('Trending Date','Title','Channel Title','Publish Time','Views','Likes','Dislikes')
+        print(primera_linea)
+        i = 0
+        while i < num_countries:
+            video = lt.getElement(videos,i)
+            t_date = video['trending_date']
+            title = video['title']
+            ch_title = video['channel_title']
+            pub_time = video['publish_time']
+            views = video['views']
+            likes = video['likes']
+            dislikes = video['dislikes']
+            info_video = "{0:^13}{1:^100}{2:^25}{3:^25}{4:^15}{5:^10}{6:^10}".format(t_date, title, ch_title, pub_time, views, likes, dislikes)
+            print(info_video)
+            i+=1
+
+def print_most_trending_country(most_trending, category):
+    """
+    Imprime el video más trending en un país específico. Muestra además su título, el canal
+    que lo publicó, el país donde fue publicado y el número de días que ha sido trending
+    """
+    size = lt.size(most_trending)
+    if size:
+        print('El video mas trending en la categoria ' + category.title() + ":")
+        primera_linea = "{0:^100}{1:^25}{2:^15}{3:^15}".format('Title','Channel Title','Pais','Numero de dias')
+        print(primera_linea)
+        i = 0
+        video = lt.getElement(most_trending,i)
+        title = video['title']
+        ch_title = video['channel_title']
+        country = video['country']
+        num_days = video['num_days']
+        info_video = "{0:^100}{1:^25}{2:^15}{3:^15}".format(title, ch_title, country, num_days)
+        print(info_video)
+            
+def print_most_trending_categories(most_trending, category):
+    """
+    Imprime el video más trending en una categoría específica. Muestra además su título, el canal
+    que lo publicó, el id de la categoría y el número de días que ha sido trending
+    """
+    size = lt.size(most_trending)
+    if size:
+        print('El video mas trending en la categoria ' + category.title() + ":")
+        primera_linea = "{0:^100}{1:^25}{2:^15}{3:^15}".format('Title','Channel Title','Category ID','Numero de dias')
+        print(primera_linea)
+        i = 0
+        video = lt.getElement(most_trending,i)
+        title = video['title']
+        ch_title = video['channel_title']
+        category_id = video['category_id']
+        num_days = video['num_days']
+        info_video = "{0:^100}{1:^25}{2:^15}{3:^15}".format(title, ch_title, category_id, num_days)
+        print(info_video)
+
+def print_video_tags(tags_by_likes,num_videos,country,tag):
+    """
+    Imprime los n videos con más likes especficando, su título, título del canal, tiempo de publicación, 
+    vistas likes, dislikes y los tags con los que está marcado el video
+    """
+    size = lt.size(tags_by_likes)
+    if size:
+        print(' Estos son los videos con más likes en ' + country.title() + " con el tag " + tag + ':')
+        primera_linea = "{0:^100}{1:^25}{2:^25}{3:^15}{4:^10}{5:^10}{6:100}".format('Title','Channel Title','Publish Time','Views','Likes','Dislikes','tags')
+        print(primera_linea)
+        i = 0
+        while i < num_videos:
+            video = lt.getElement(tags_by_likes,i)
+            tags= video['tags']
+            title = video['title']
+            ch_title = video['channel_title']
+            pub_time = video['publish_time']
+            views = video['views']
+            likes = video['likes']
+            dislikes = video['dislikes']
+            info_video = "{0:^100}{1:^25}{2:^25}{3:^15}{4:^10}{5:^10}{6:100}".format(title, ch_title, pub_time, views, likes, dislikes, tags)
+            print(info_video)
+            i+=1
 """
 Menu principal
 """
 while True:
     printMenu()
-    inputs = input('Selecci1one una opción para continuar\n')
+    inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
         print("Cargando información de los archivos ....")
         tipo = int(input("Ingrese 1 si desea un arreglo o cualquier otro caracter para una lista encadenada: "))
@@ -77,11 +198,31 @@ while True:
               str(lt.size(catalog['Videos'])))
 
     elif int(inputs[0]) == 2:
-        size = int(input("Seleccione el tamaño de la muestra: "))
-        print("\nSeleccione el tipo de ordenamiento:\n-1 para insertion\n-2 para selection\n-3 para shellshort\n-4 para quickshort\n-5 para mergeshort")
-        sort = int(input("Ingrese su eleccion: "))
-        lista_videos = controller.Videosxviews(catalog,sort,size)
+        num_countries = int(input("Escriba en numeros la cantidad de videos que desea consultar: "))
+        country = input("Escriba el país sobre el que desea hacer la consulta: ").lower()
+        category = input("Ingrese la categoria que desea buscar: ")
+        size = lt.size(catalog['Videos'])
+        #print("\nSeleccione el tipo de ordenamiento:\n-1 para insertion\n-2 para selection\n-3 para shellshort\n-4 para quickshort\n-5 para mergeshort")
+        #sort = int(input("Ingrese su eleccion: "))
+        sort = 5
+        lista_videos = controller.videos_by_views(catalog,sort,size)
         print(lista_videos[0])
+        views_country_category(lista_videos[1],country,num_countries,category)
+
+    elif int(inputs[0]) == 3:
+        country = input("Ingrese el pais en el que desea buscar: ")
+        trending_country(catalog['Videos'],country)
+
+    elif int(inputs[0]) == 4:
+        category = input("Ingrese la categoria en la que desea buscar: ")
+        trending_category(catalog['Videos'],category)
+
+    elif int(inputs[0]) == 5:
+        num_videos = int(input("Escriba en numeros la cantidad de videos que desea consultar: "))
+        country = input("Ingrese el pais en el que desea buscar: ")
+        tag = input("Ingrese el tag que desea buscar: ")
+        likes_country_tag(num_videos,country,tag,catalog['Videos'])
+
     else:
         sys.exit(0)
 sys.exit(0)
